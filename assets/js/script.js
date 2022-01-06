a638ce16ca1537155ee2ee8d7ca1ebc3661f12e2
Gwen Ewasko <gwenewasko@gmail.com>
2022-01-06 08:55:47 -0600

    getting randomize to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,55 +2,57 @@
 let generateBtn = document.querySelector("#generate");
 
 // 1b. create an array of letters and special characters
-let lcLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"];
-let ucLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
-let pwNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-let specialCharacters = ["!", "@", "#", "%", "&", "*", "?"];
+let lcLetters = "abcdefghijklmnopkrstuvwxyz";
+let ucLetters = "ABC";
+let pwNumbers = "123";
+let specialCharacters = "!@#";
+let password = [];
 
 // generate password
 // return a final password as a string
 function generatePassword() {
   // 1. assign a variable to length of the password
-  let password = [];
+  let wordBank = "";
   let passwordLength = prompt("Please chooose a password length.");
-  let minLength = 4;
-  let maxLength = 128;
+  // let minLength = 4;
+  // let maxLength = 128;
 
-  // 1a. create a pw with that length
-  if (passwordLength < 4 || passwordLength > 128) {
+  // pw length validation
+  if (passwordLength < 8 || passwordLength > 128) {
     alert("Please choose a password length between 8 and 128 charachters.");
     generatePassword();
-  } else {
-    let trueLc = confirm("Would you like to use lower case letters?");
-    let trueUp = confirm("Would you like to use upper case letters?");
-    let trueNumbers = confirm("Would you like to use numbers?");
-    let trueSpecChar = confirm("Would you like to use special characters?");
-
-    // 1c. loop through array of letters
-    if (trueLc === true) {
-      password = password.concat(lcLetters);
-      // password.push(getGenerateRandom(lcLetters));
-      // pwLength--;
-    }
-    if (trueUp === true) {
-      password = password.concat(ucLetters);
-      // password.push(getGenerateRandom(ucLetters));
-    }
-    if (trueNumbers === true) {
-      password = password.concat(pwNumbers);
-    }
-    if (trueSpecChar === true) {
-      password = password.concat(specialCharacters);
-    }
-
-    // for (let i = 0; i < passwordLength; i++) {
-    // password.push(lcLetters[i]);
-
-    return password.join("");
   }
+  let trueLc = confirm("Would you like to use lower case letters?");
+  let trueUp = confirm("Would you like to use upper case letters?");
+  let trueNumbers = confirm("Would you like to use numbers?");
+  let trueSpecChar = confirm("Would you like to use special characters?");
+
+  // 1c. loop through array of letters
+  if (trueLc) {
+    wordBank += lcLetters;
+    // password.push(getGenerateRandom(lcLetters));
+    // pwLength--;
+  }
+  if (trueUp) {
+    wordBank += ucLetters;
+    // password.push(getGenerateRandom(ucLetters));
+  }
+  if (trueNumbers) {
+    wordBank += pwNumbers;
+    // password = password.concat(pwNumbers);
+  }
+  if (trueSpecChar) {
+    wordBank += specialCharacters;
+    // password = password.concat(specialCharacters);
+  }
+  var randomLetter = generateRandom(wordBank.split(""));
+  // // for (let i = 0; i < passwordLength; i++) {
+  password.push(randomLetter);
+
+  return password.join("");
 }
 function generateRandom(characters) {
-  let index = Math.floor(math.random() * characters.length);
+  let index = Math.floor(Math.random() * characters.length);
   return characters[index];
 }
 // 1d. push each letter arry into pw array
